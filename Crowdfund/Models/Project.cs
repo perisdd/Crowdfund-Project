988b7a8253a423a6fa8d817dcb88e18029e1e3a7
perisdd <ntinos.periklis@gmail.com>
2022-11-30 14:37:19 +0200

    Add Project Model

--- Crowdfund/Models/Project.cs
+++ Crowdfund/Models/Project.cs
@@ -2,6 +2,34 @@
 {
     public class Project
     {
+        public int Id { get; set; }
 
+        public string Title { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public Creator? Creator { get; set; }
+
+        public Category ProjectCategory { get; set; }
+
+		public decimal Contributions { get; set; } = 0.00m;
+
+		public decimal Goal { get; set; } = 0.00m;
+
+        public List<Reward> Rewards { get; set; } = new List<Reward>();
+
+        public List<Backer> Backers { get; set; } = new List<Backer>();
+
+        public List<string> StatusUpdates { get; set; } = new List<string>();
+
+        public enum Category
+        {
+            Arts,
+            Environment,
+            Film,
+            Games,
+            Music,
+            Technology
+        }
     }
 }
