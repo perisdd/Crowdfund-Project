bbb3d703d4cda10726601c2d3a90614b74cb293d
mnint <mnintsiou@athtech.gr>
2022-12-02 15:04:39 +0200

    Added an enum in the User (to distinguish Backer and Creator)

--- Crowdfund/Models/User.cs
+++ Crowdfund/Models/User.cs
@@ -10,5 +10,14 @@
         public string UserName { get; set; } = "";
 
         public string Password { get; set; } = "";
+
+        public UserType Role { get; set; } 
+
     }
+
+    public enum UserType 
+    {
+        BACKER,
+        CREATOR
+    } 
 }
