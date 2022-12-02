797a290cf4cdde065ce88d31cafd4e0a5f98b5bc
Marjus Boci <mbotsis@athtech.gr>
2022-12-02 16:23:27 +0200

    Creator-Model

--- Crowdfund/Models/Creator.cs
+++ Crowdfund/Models/Creator.cs
@@ -1,7 +1,17 @@
 ﻿namespace Crowdfund.Models
 {
-    public class Creator
+    public class Creator: User
     {
+        public User? _creator { get; set; }
+        List<Project>? projectsCreated { get; set; }
 
+        public void CreateProject() { }
+        public void AddProjectTitle() { }
+        public void AddDescription() { }
+        public void AddPhotos() { }
+        public void AddVideos() { }
+        public void PostStatusUpdates() { }
+        public void TrackFinancialProgress() { }
+        public void OfferFundingPackages() { }
     }
 }
