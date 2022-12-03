a56c7f87e1f52bbeeecde6339e3c9fe8dfe28b74
mnint <mnintsiou@athtech.gr>
2022-12-04 00:52:25 +0200

    Added Http Routes for each new CRUD function

--- Crowdfund-API/Controllers/ProjectsController.cs
+++ Crowdfund-API/Controllers/ProjectsController.cs
@@ -33,12 +33,51 @@ namespace Crowdfund_API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProjectDTO>> Post(ProjectDTO projectDTO)
+        public async Task<ActionResult<ProjectDTO?>> Post(ProjectDTO? projectDTO)
         {
+            if (projectDTO is null) return BadRequest("Please provide a valid project");
+            
             ProjectDTO result = await _service.AddProject(projectDTO);
-            if (result == null) { return NotFound("Invalid Creator ID."); }
+  
+            if (result is null) return NotFound("Invalid Creator ID.");
 
             return Ok(result);
         }
+
+
+        [HttpGet, Route("search")]
+        public async Task<ActionResult<List<ProjectDTO?>>> Search(string? title, string? creatorFirst, string? creatorLast)
+        {
+            var result = await _service.SearchByName(title, creatorFirst, creatorLast);
+            if (result is null || !result.Any()) return BadRequest("No projects that match the specified criteria were found.");
+
+            return Ok(result);
+        }
+
+
+        [HttpPatch, Route("update")]
+        public async Task<ActionResult<ProjectDTO?>> Update(int id, ProjectDTO projectDTO)
+        {
+
+            var result = await _service.UpdateProject(id,projectDTO);
+
+            if (result is null) return BadRequest("No previous project was found to update.");
+
+            return Ok("Project updated successfully!");
+        }
+
+
+        [HttpDelete, Route("delete")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+
+            var result = await _service.DeleteProject(id);
+
+            if (result) return Ok("Project deleted successfully!");
+
+            return BadRequest("No project was found to delete.");
+         
+        }
+
     }
 }
