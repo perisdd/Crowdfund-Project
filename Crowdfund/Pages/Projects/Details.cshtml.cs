0070feb9b48e0689e9e6b8692dac293f5a78780a
Mihalis-P <mixalhs_mike@hotmail.com>
2022-12-05 00:19:43 +0200

    Added Layout for Creators + Backers + Project Details Page

---
+++ Crowdfund/Pages/Projects/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Crowdfund.Pages.Projects
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
