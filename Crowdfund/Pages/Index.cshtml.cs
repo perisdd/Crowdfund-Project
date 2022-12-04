6885b9ce4f2e7fedce6734314309b529c07de86e
perisdd <ntinos.periklis@gmail.com>
2022-12-04 23:44:18 +0200

    UI Minor Changes

--- Crowdfund/Pages/Index.cshtml.cs
+++ Crowdfund/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Crowdfund.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Crowdfund.Pages
@@ -6,6 +7,8 @@ namespace Crowdfund.Pages
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
+
+		public List<string> Categories = Enum.GetNames(typeof(Category)).ToList();
 
 		public IndexModel(ILogger<IndexModel> logger)
 		{
