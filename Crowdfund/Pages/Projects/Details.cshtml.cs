a4c1b1706b1720ce16b7d373dc9ee0c4d0b8a4cd
Mihalis-P <mixalhs_mike@hotmail.com>
2022-12-05 17:20:34 +0200

    Added Money Goal[Migration Needed] + Details Page template

--- Crowdfund/Pages/Projects/Details.cshtml.cs
+++ Crowdfund/Pages/Projects/Details.cshtml.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.InteropServices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Crowdfund.Models;
+using Crowdfund.DB;
 
 namespace Crowdfund.Pages.Projects
 {
