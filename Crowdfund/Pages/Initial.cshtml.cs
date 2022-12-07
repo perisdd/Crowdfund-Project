0f017967e3351c5623201a177cfe6f1880a4e1ab
perisdd <ntinos.periklis@gmail.com>
2022-12-07 11:46:14 +0200

    Choose Creator Pulls From DB instead of Static Data

--- Crowdfund/Pages/Initial.cshtml.cs
+++ Crowdfund/Pages/Initial.cshtml.cs
@@ -1,3 +1,5 @@
+using Crowdfund.DB;
+using Crowdfund.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,15 +9,26 @@ namespace Crowdfund.Pages
     {
 		public static int test { get; set; }
 
-        public IActionResult OnPost(int creators)
+		private FundDbContext Context { get; }
+
+		public List<Creator> Creators { get; set; }
+		public List<Backer> Backers { get; set; }
+
+		public InitialModel(FundDbContext context)
+		{
+			Context = context;
+		}
+
+		public IActionResult OnPost(int creators)
         {
             test = creators;
             return RedirectToPage("Projects/Create");
         }
+		public void OnGet()
+		{
+			Backers = Context.Backers.ToList();
+			Creators = Context.Creators.ToList();
 
-       public void OnGet()
-        {
-
-        }
+		}
     }
 }
