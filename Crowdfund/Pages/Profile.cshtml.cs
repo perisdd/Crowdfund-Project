28c789001f0c226f8b9733cd8d4a20938a8ce262
perisdd <ntinos.periklis@gmail.com>
2022-12-07 21:04:04 +0200

    Layout Clean-Up (4 Common Lines Everywhere - Moved to _MyLayout)

--- Crowdfund/Pages/Profile.cshtml.cs
+++ Crowdfund/Pages/Profile.cshtml.cs
@@ -7,25 +7,33 @@ namespace Crowdfund.Pages
 {
 	public class ProfileModel : PageModel
 	{
-		private FundDbContext Context { get; }
+		public Backer? Backer { get; set; }
 
 		public Creator? Creator { get; set; }
 
-		public Backer? Backer { get; set; }
+		private FundDbContext Context { get; }
 
 		public ProfileModel(FundDbContext context)
 		{
 			Context = context;
 		}
-
-		private readonly int current = InitialModel.CurrentId;
 		
 		public void OnGet()
 		{
 			if (InitialModel.CurrentRole.Equals("Creator"))
-				Creator = Context.Creators.Include(c => c.ProjectsCreated).SingleOrDefault(c => c.Id == current);
+			{
+				Creator = Context.Creators
+					.Include(c => c.ProjectsCreated)
+					.SingleOrDefault(c => c.Id == InitialModel.CurrentId);
+			}
+
 			else if (InitialModel.CurrentRole.Equals("Backer"))
-				Backer = Context.Backers.Include(b => b.ProjectsInvested).Include(b => b.Contributions).SingleOrDefault(b => b.Id == current);
+			{
+				Backer = Context.Backers
+					.Include(b => b.ProjectsInvested)
+					.Include(b => b.Contributions)
+					.SingleOrDefault(b => b.Id == InitialModel.CurrentId);
+			}
 		}
 	}
 }
