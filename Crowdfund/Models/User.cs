fc7a4e4ee1d37a651ba187330c02bd6430ef413c
Mihalis-P <mixalhs_mike@hotmail.com>
2022-12-08 01:04:22 +0200

    Backer+Creator Profile Picture| Migration Needed!

--- Crowdfund/Models/User.cs
+++ Crowdfund/Models/User.cs
@@ -13,6 +13,8 @@ namespace Crowdfund.Models
         [Required]
         public string? Email { get; set; }
 
+        public string? ImgUrl { get; set; }
+
         public string? UserName { get; set; }
 
         public string? Password { get; set; }
