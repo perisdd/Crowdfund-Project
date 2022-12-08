39a9d4d107de8276e475fb1a317cd6311461880a
mnint <mnintsiou@athtech.gr>
2022-12-08 17:41:11 +0200

    Added BackerId and ProjectId and validations

--- Crowdfund/Models/Contribution.cs
+++ Crowdfund/Models/Contribution.cs
@@ -1,15 +1,25 @@
-﻿namespace Crowdfund.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crowdfund.Models
 {
     public class Contribution
     {
         public int Id { get; set; }
+
+        [Required]
         public decimal? Amount { get; set; }
 
         public Backer? Backer { get; set; }
  
         public Project? Project { get; set; }
 
-    //public List<Project> Projects { get; set; }
-    //public List<Backer>? Backers { get; set; }
-}
+        [Required]
+        public int BackerId { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        //public List<Project> Projects { get; set; }
+        //public List<Backer>? Backers { get; set; }
+    }
 }
