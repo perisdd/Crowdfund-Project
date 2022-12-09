bc0b8479af884b6e9145cca81995b30f6c41b0b1
mnint <mnintsiou@athtech.gr>
2022-12-09 18:21:26 +0200

    Added Delete functionality for Backers and Creators

---
+++ Crowdfund/Pages/Backers/Delete.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Crowdfund.DB;
+using Crowdfund.Models;
+
+namespace Crowdfund.Pages.Backers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly FundDbContext _context;
+
+
+        [BindProperty] public Backer Backer { get; set; }
+
+
+        public DeleteModel(FundDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Backers == null)
+            {
+                return NotFound();
+            }
+
+            var backer = await _context.Backers
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (backer == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                 Backer = backer;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Backers == null)
+            {
+                return NotFound();
+            }
+            var backer = await _context.Backers.FindAsync(id);
+
+            if (backer != null)
+            {
+                Backer = backer;
+                _context.Backers.Remove(backer);
+                await _context.SaveChangesAsync();
+            }
+
+            TempData["AlertMessage"] = "Backer Deleted Successfully!";
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Crowdfund/Pages/Creators/Delete.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Crowdfund.DB;
+using Crowdfund.Models;
+
+namespace Crowdfund.Pages.Creators
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly FundDbContext _context;
+
+
+        [BindProperty] public Creator Creator { get; set; }
+
+
+        public DeleteModel(FundDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Creators == null)
+            {
+                return NotFound();
+            }
+
+            var creator = await _context.Creators
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (creator == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                 Creator = creator;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Creators == null)
+            {
+                return NotFound();
+            }
+            var creator = await _context.Creators.FindAsync(id);
+
+            if (creator != null)
+            {
+                Creator = creator;
+                _context.Creators.Remove(creator);
+                await _context.SaveChangesAsync();
+            }
+
+            TempData["AlertMessage"] = "Project Deleted Successfully!";
+            return RedirectToPage("./Index");
+        }
+    }
+}
